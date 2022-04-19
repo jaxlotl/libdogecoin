48110d4420a9359d13c6044ca8637535ae88e979
jaxlotl <jackie.mcaninch.2019@gmail.com>
2022-04-19 14:36:18 -0700

    simplified setup.py, no longer requires manylinux

--- setup.py
+++ setup.py
@@ -1,49 +1,14 @@
-#! /usr/bin/env python3.8
-from setuptools import setup, Extension
-
-SOURCES = ["src/address.c",
-           "src/crypto/aes.c",
-           "src/crypto/base58.c",
-           "src/bip32.c",
-           "src/buffer.c",
-           "src/chainparams.c",
-           "src/cstr.c",
-           "src/crypto/ecc.c",
-           "src/crypto/key.c",
-           "src/mem.c",
-           #"src/crypto/random.c",
-           "src/crypto/rmd160.c",
-           "src/script.c",
-           "src/crypto/segwit_addr.c",
-           "src/serialize.c",
-           "src/crypto/sha2.c",
-           #"src/cli/such.c",
-           "src/cli/tool.c",
-           "src/tx.c",
-           "src/utils.c",
-           "src/vector.c"]
-
-INCLUDES = ["include/",
-            "src/"]
-
-MACROS = [("RANDOM_DEVICE", "/dev/urandom")]
-
-C_LIB = Extension(name=                 "libdogecoin",
-                  sources=              SOURCES,
-                  include_dirs=         INCLUDES,
-                  define_macros=        MACROS,
-                  libraries=            ["dogecoin"],
-                  runtime_library_dirs= [".libs"])
-
+from setuptools import setup
 
 setup(
     name=               "libdogecoin",
-    version=            "0.1",
+    version=            "0.3",
     description=        "Python interface for the libdogecoin C library",
     author=             "Jackie McAninch",
     author_email=       "jackie.mcaninch.2019@gmail.com",
     license=            "MIT",
     python_requires=    ">=3.8.10",
-    ext_modules=        [C_LIB],
-    packages=           ["bindings/py_wrappers/libdogecoin"]
-    )
+    packages=           ["libdogecoin"],
+    package_dir=        {"":"bindings/py_wrappers"},
+    package_data=       {"": ["libdogecoin.so"]}
+)
